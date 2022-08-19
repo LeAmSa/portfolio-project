a329433d9dc825f62a8f64e64a6e5062339d527b
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-08-19 08:57:14 -0300

    Initial commit

---
+++ scripts/swiper.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".main-swiper", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 0,
+    mousewheel: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
