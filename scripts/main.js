3ff3c4f0a10e487a5b2b81896473bf36c594c3bb
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-08-23 16:36:45 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ const qrcodeCt = document.getElementsByClassName('qrcode-ct')
 const btnQrOpen = document.getElementsByClassName('btn-qr-open')
 const btnQrClose = document.getElementsByClassName('btn-qr-close')
 
+//Open and close qrcode images
 for (let i = 0; i < qrcodeCt.length; i++) {
     btnQrOpen[i].addEventListener('click', () => {
         qrcodeCt[i].classList.add('active')
@@ -11,5 +12,4 @@ for (let i = 0; i < qrcodeCt.length; i++) {
         qrcodeCt[i].classList.remove('active')
     })
 }
-
 
