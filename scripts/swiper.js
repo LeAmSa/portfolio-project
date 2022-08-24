89800944651a70fa3f1932c9de4f37832c325ed1
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2022-08-24 16:52:47 -0300

    Add background animations function

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,5 +1,5 @@
 var swiper = new Swiper(".main-swiper", {
-    speed: 600,
+    speed: 700,
     direction: "vertical",
     slidesPerView: 1,
     spaceBetween: 0,
@@ -12,6 +12,7 @@ var swiper = new Swiper(".main-swiper", {
 
 //Nav item go to associated slide and change nav item class
 const navItems = document.getElementsByTagName('li');
+const mainCt = document.querySelector('.main-ct');
 
 for (let i = 0; i < navItems.length; i++) {
   navItems[i].addEventListener('click', function() {
@@ -22,7 +23,36 @@ for (let i = 0; i < navItems.length; i++) {
   });
 }
 
-// associating nav item with the slide active on scroll or bullet click
+// associating nav item with the slide active on scroll or bullet click and changing background gradients
 swiper.on('slideChange', function () {
-  navItems[swiper.activeIndex].click()
+  navItems[swiper.activeIndex].click();
+
+  switch (swiper.activeIndex) {
+    case 0:
+      mainCt.classList.remove('slide2', 'slide3', 'slide4', 'slide5');
+      break;
+  
+    case 1:
+      mainCt.classList.remove('slide3', 'slide4', 'slide5');
+      mainCt.classList.add("slide2");
+      break;
+      
+    case 2:
+      mainCt.classList.remove('slide2', 'slide4', 'slide5');
+      mainCt.classList.add("slide3");
+      break;
+
+    case 3:
+      mainCt.classList.remove('slide2', 'slide3', 'slide5');
+      mainCt.classList.add("slide4");
+      break;
+
+    case 4:
+      mainCt.classList.remove('slide2', 'slide3', 'slide4');
+      mainCt.classList.add("slide5");
+      break;
+
+    default:
+      break;
+  }
 });
